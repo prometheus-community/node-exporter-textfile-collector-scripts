e88b09cac621729cc6441d907fefae6dcb087ac4
Linus Heckemann <git@sphalerite.org>
2020-12-29 14:15:14 +0100

    smartmon.py: use "device" for block device, "disk" for physical disk
    Signed-off-by: Linus Heckemann <git@sphalerite.org>

--- smartmon.py
+++ smartmon.py
@@ -86,8 +86,8 @@ class Device(collections.namedtuple('DeviceBase', 'path opts')):
     @property
     def base_labels(self):
         if '+' in self.type:
-            return {'dev': self.path, 'disk': self.type.partition("+")[2]}
-        return {'disk': self.path}
+            return {'device': self.path, 'disk': self.type.partition("+")[2]}
+        return {'device': self.path}
 
     def smartctl_select(self):
         return ['--device', self.type, self.path]
