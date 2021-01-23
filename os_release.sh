19a7009bb5c049695d54b8e661e600361d7c57fb
Darshil Chanpura <dtchanpura@protonmail.com>
2021-01-23 12:21:32 +0530

    os_release.sh: removed unnecessary bash substitution
    Signed-off-by: Darshil Chanpura <dtchanpura@protonmail.com>

--- os_release.sh
+++ os_release.sh
@@ -13,4 +13,4 @@ set -o pipefail
 
 echo "# HELP node_os_info A metric with a constant '1' value labeled by OS Release id, id_like, name, pretty_name, version, version_codename and version_id"
 echo "# TYPE node_os_info gauge"
-echo "node_os_info{id=\"${ID: }\",id_like=\"${ID_LIKE: }\",name=\"${NAME: }\",pretty_name=\"${PRETTY_NAME: }\",version=\"${VERSION: }\",version_codename=\"${VERSION_CODENAME: }\",version_id=\"${VERSION_ID: }\"} 1"
+echo "node_os_info{id=\"${ID}\",id_like=\"${ID_LIKE}\",name=\"${NAME}\",pretty_name=\"${PRETTY_NAME}\",version=\"${VERSION}\",version_codename=\"${VERSION_CODENAME}\",version_id=\"${VERSION_ID}\"} 1"
