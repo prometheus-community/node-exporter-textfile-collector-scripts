24c7be0a1ee0505178802de38f0bfba8932c148b
gnanasalten <69344036+gnanasalten@users.noreply.github.com>
2022-09-26 20:26:37 +0530

    Create fstab-check.sh (#128)
    * Create fstab-check.sh
    
    Script to check the filesystems are mounted as per /etc/fstab entry
    
    Signed-off-by: gnanasalten <69344036+gnanasalten@users.noreply.github.com>

---
+++ fstab-check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "# HELP node_fstab_mount_status List and status of filesystem mountpoints (0 = not mounted, 1 = mounted)"
+echo "# TYPE node_fstab_mount_status gauge"
+mapfile -t mountpoints < <(awk '$1 !~ /^#/ && $2 ~ /^[/]/ {print $2}' /etc/fstab)
+for mount in "${mountpoints[@]}"
+do
+  if ! findmnt "$mount" &> /dev/null
+  then
+    echo "node_fstab_mount_status{mountpoint=\"$mount\"} 0"
+  else
+    echo "node_fstab_mount_status{mountpoint=\"$mount\"} 1"
+  fi
+done
