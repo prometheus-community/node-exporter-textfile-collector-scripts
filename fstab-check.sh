21ee9132c36f76c5ccbeb4ff4c1f0718efb0ff14
gnanasalten <69344036+gnanasalten@users.noreply.github.com>
2022-09-26 20:14:11 +0530

    Update fstab-check.sh
    Signed-off-by: gnanasalten <69344036+gnanasalten@users.noreply.github.com>

--- fstab-check.sh
+++ fstab-check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "# HELP node_fstab_mount_status List and status of filesystem mountpoints"
+echo "# TYPE node_fstab_mount_status gauge"
 mapfile -t mountpoints < <(awk '$1 !~ /^#/ && $2 ~ /^[/]/ {print $2}' /etc/fstab)
 for mount in "${mountpoints[@]}"
 do
