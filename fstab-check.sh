3a7b13d335758a1c2d9909d36b8bb09081e4160b
gnanasalten <69344036+gnanasalten@users.noreply.github.com>
2022-09-26 19:21:42 +0530

    Update fstab-check.sh
    Signed-off-by: gnanasalten <69344036+gnanasalten@users.noreply.github.com>

--- fstab-check.sh
+++ fstab-check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-> /var/lib/node_exporter/fstab-check.prom
-mountpoints=( $(awk '$1 !~ /^#/ && $2 ~ /^[/]/ {print $2}' /etc/fstab) )
+path="/var/lib/node_exporter/fstab-check.prom"
+mapfile -t mountpoints < <(awk '$1 !~ /^#/ && $2 ~ /^[/]/ {print $2}' /etc/fstab)
 for mount in "${mountpoints[@]}"
 do
    if ! findmnt "$mount" &> /dev/null
    then
-         echo "node_fstab_mount_status{filesystem=\"$mount\"} 0" >> /var/lib/node_exporter/fstab-check.prom
+         echo "node_fstab_mount_status{filesystem=\"$mount\"} 0" | sponge $path
    else
-         echo "node_fstab_mount_status{filesystem=\"$mount\"} 1" >> /var/lib/node_exporter/fstab-check.prom
+         echo "node_fstab_mount_status{filesystem=\"$mount\"} 1" | sponge $path
    fi
 done
