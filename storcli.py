fa26153da8d77806d2f59b34f9adc26074e3da1b
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2023-05-19 22:45:56 +0200

    Construct storcli_cmd in a more Python way
    Signed-off-by: Daniel Swarbrick <daniel.swarbrick@gmail.com>

--- storcli.py
+++ storcli.py
@@ -239,7 +239,9 @@ def get_storcli_json(storcli_args):
     # Check if storcli is installed and executable
     if not (os.path.isfile(storcli_path) and os.access(storcli_path, os.X_OK)):
         SystemExit(1)
-    storcli_cmd = shlex.split(storcli_path + ' ' + storcli_args + ' nolog')
+    storcli_cmd = [storcli_path]
+    storcli_cmd.extend(shlex.split(storcli_args))
+    storcli_cmd.append('nolog')
     proc = subprocess.Popen(
         storcli_cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output_json = proc.communicate()[0]
