c0e3659fb9d9439477001548e8fc6bc0081a2f37
Andrew Roberts <adroberts@gmail.com>
2025-05-12 22:40:18 -0400

    [directory-size.sh] support spaces in paths
    Signed-off-by: Andrew Roberts <adroberts@gmail.com>

--- directory-size.sh
+++ directory-size.sh
@@ -6,9 +6,11 @@
 #
 # */5 * * * * prometheus directory-size.sh /var/lib/prometheus | sponge /var/lib/node_exporter/directory_size.prom
 #
+# awk logic from by https://stackoverflow.com/a/10221507
+#
 # Author: Antoine Beaupré <anarcat@debian.org>
 
 echo "# HELP node_directory_size_bytes Disk space used by some directories"
 echo "# TYPE node_directory_size_bytes gauge"
 du --block-size=1 --summarize "$@" \
-  | awk '{ print "node_directory_size_bytes{directory=\"" $2 "\"} "  $1 }'
+  | awk '{ sz = $1; $1 = ""; print "node_directory_size_bytes{directory=\"" substr($0, 2) "\"} "  sz }'
