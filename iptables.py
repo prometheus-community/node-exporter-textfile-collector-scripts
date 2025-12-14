32f983409dd55f76e96a6b889c0554aad88f934f
Monica Hanson <mhanson@monicarose.tech>
2025-12-14 11:01:23 -0600

    Add line number support
    Signed-off-by: Monica Hanson <mhanson@monicarose.tech>

--- iptables.py
+++ iptables.py
@@ -6,8 +6,8 @@ import os
 
 tables = ['filter', 'nat', 'mangle', 'raw']
 re_chain = re.compile('^Chain')
-re_header = re.compile('^pkts')
-re_blankline = re.compile('^(?:^ *\n)+$')
+re_header = re.compile('^num')
+re_blankline = re.compile('^(?:^ *\n)$')
 
 iptables_packet_lines = []
 iptables_byte_lines = []
@@ -19,7 +19,8 @@ for table in tables:
   # -v: Verbose output
   # -x: Exact values
   # -t table: Specified table table
-  cmd = ['/sbin/iptables', '-L', '-n', '-v', '-x', '-t', table]
+  # --line-numbers: Show line numbers
+  cmd = ['/sbin/iptables', '-L', '-n', '-v', '-x', '-t', table, "--line-numbers"]
   proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
   for line in proc.stdout.readlines():
     line = line.decode('utf8')
@@ -38,18 +39,19 @@ for table in tables:
     if re_header.match(str(line_pieces[0])):
       continue
 
-    l_packets = line_pieces[0]
-    l_bytes = line_pieces[1]
-    l_target = line_pieces[2]
-    l_prot = line_pieces[3]
-    l_in = line_pieces[5]
-    l_out = line_pieces[6]
-    l_src = line_pieces[7]
-    l_dest = line_pieces[8]
-    l_options = ' '.join(line_pieces[9:]).replace('"','\\"')
+    l_line_number = line_pieces[0]
+    l_packets = line_pieces[1]
+    l_bytes = line_pieces[2]
+    l_target = line_pieces[3]
+    l_prot = line_pieces[4]
+    l_in = line_pieces[6]
+    l_out = line_pieces[7]
+    l_src = line_pieces[8]
+    l_dest = line_pieces[9]
+    l_options = ' '.join(line_pieces[10:]).replace('"','\\"')
 
-    iptables_packet_lines.append('iptables_packets_total{table="%s",chain="%s",target="%s",prot="%s",in="%s",out="%s",src="%s",dest="%s",opt="%s"} %s' % (table,l_chain_name,l_target,l_prot,l_in,l_out,l_src,l_dest,l_options,l_packets))
-    iptables_byte_lines.append('iptables_bytes_total{table="%s",chain="%s",target="%s",prot="%s",in="%s",out="%s",src="%s",dest="%s",opt="%s"} %s' % (table,l_chain_name,l_target,l_prot,l_in,l_out,l_src,l_dest,l_options,l_bytes))
+    iptables_packet_lines.append('iptables_packets_total{table="%s",chain="%s",line_number=%s,target="%s",prot="%s",in="%s",out="%s",src="%s",dest="%s",opt="%s"} %s' % (table,l_chain_name,l_line_number,l_target,l_prot,l_in,l_out,l_src,l_dest,l_options,l_packets))
+    iptables_byte_lines.append('iptables_bytes_total{table="%s",chain="%s",line_number=%s,target="%s",prot="%s",in="%s",out="%s",src="%s",dest="%s",opt="%s"} %s' % (table,l_chain_name,l_line_number,l_target,l_prot,l_in,l_out,l_src,l_dest,l_options,l_bytes))
 
 print('# HELP iptables_packets_total packet counters for iptable rules.')
 print('# TYPE iptables_packets_total counter')
