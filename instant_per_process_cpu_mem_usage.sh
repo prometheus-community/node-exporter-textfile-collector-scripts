a7e37a4d014b1b334e95d381f069ed84b224a14f
Orsiris de Jong <ozy@netpower.fr>
2026-05-28 00:27:54 +0200

    Remove pids from process list because of high entropy
    Signed-off-by: Orsiris de Jong <ozy@netpower.fr>

--- instant_per_process_cpu_mem_usage.sh
+++ instant_per_process_cpu_mem_usage.sh
@@ -13,17 +13,17 @@ printf "# TYPE top_process_memory_usage gauge\n# HELP top_process_memory_usage p
 
 if [ "$1" == "" ]; then
 
-ps -ax -o pid,%cpu,%mem,comm,args --cols 80 | awk '{
-        args=""; for(i = 5; i<= NF; i++) if ($i!="") {args=args" "$i};
+ps -ax -o %cpu,%mem,comm,args --cols 80 | awk '{
+        args=""; for(i = 4; i<= NF; i++) if ($i!="") {args=args" "$i};
         gsub("{|}|\\\\|\"", "", args);
         # Remove self process
-        if ($4=="ps" && args=" -ax -o pid,%cpu,%mem,comm,args --cols 80") { next };
+        if ($3=="ps" && args=" -ax -o %cpu,%mem,comm,args --cols 80") { next };
         # Do not keep not cpu hungry entries
-        if ($2!="0.0") {
-                printf "top_process_cpu_usage{pid=\""$1"\",process=\""$4"\",args=\""args"\"} " $2z"\n"};
+        if ($1!="0.0") {
+                printf "top_process_cpu_usage{process=\""$3"\",args=\""args"\"} " $1z"\n"};
         # Do not keep not memory hungry entries
-        if ($3!="0.0") {
-                printf "top_process_memory_usage{pid=\""$1"\",process=\""$4"\",args=\""args"\"} " $3z"\n"};
+        if ($2!="0.0") {
+                printf "top_process_memory_usage{process=\""$3"\",args=\""args"\"} " $2z"\n"};
 }'
 
 elif [ "$1" == "top" ]; then
@@ -48,10 +48,10 @@ top -w 120 -cbn 1 | awk '{
         if ($12=="top" && args=" -w 120 -cbn 1") { next };
         # Do not keep not cpu hungry entries
         if ($9!="0.0") {
-                printf "top_process_cpu_usage{pid=\""$1"\",process=\""$12"\",args=\""args"\"} " $9z"\n"};
+                printf "top_process_cpu_usage{process=\""$12"\",args=\""args"\"} " $9z"\n"};
         # Do not keep not memory hungry entries
         if ($10!="0.0") {
-                printf "top_process_memory_usage{pid=\""$1"\",process=\""$12"\",args=\""args"\"} " $10z"\n"};
+                printf "top_process_memory_usage{process=\""$12"\",args=\""args"\"} " $10z"\n"};
 }'
 
 fi
