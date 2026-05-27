1e100ca7e3e6908880581455c4b4b32b4b7f159f
Orsiris de Jong <ozy@netpower.fr>
2026-05-27 23:27:22 +0200

    Optimize awk loop, always show prometheus headers
    Signed-off-by: Orsiris de Jong <ozy@netpower.fr>

--- instant_per_process_cpu_mem_usage.sh
+++ instant_per_process_cpu_mem_usage.sh
@@ -2,17 +2,22 @@
 
 # Get CPU & Mem metrics, lightweight implementation
 # Written (quickly) in 2023 by NetInvent
-# SCRIPT_VERSION 2023110201
+# SCRIPT_VERSION 2026052701
 
+# top represents roughly 95% of the time spent by this script
+# awk doesn't need optimisations here
 
 # -w [n] forces total column width to [n] so data won't be truncated
 # -c forces full commandline, which we need in order to get command arguments
 # -bn 1 makes top run once in batch mode
 
+printf "# TYPE top_process_cpu_usage gauge\n# HELP top_process_cpu_usage ps gathered instant CPU usage per process\n"
+printf "# TYPE top_process_memory_usage gauge\n# HELP top_process_memory_usage ps gathered memory usage per process\n"
+
 top -w 120 -cbn 1 | awk '{
         # Skip headers
         if (NR<8) { next };
-        # Get all command arguments
+        # Get all command arguments and sanitize them, avoid making empty string if no arguments found
         args=""; for(i = 13; i<= NF; i++) if ($i!="") {args=args" "$i};
         # Sanitize arguments
         gsub("{|}|\\\\|\"", "", args);
@@ -25,10 +30,8 @@ top -w 120 -cbn 1 | awk '{
         if ($12=="top" && args=" -w 120 -cbn 1") { next };
         # Do not keep not cpu hungry entries
         if ($9!="0.0") {
-                if (cputype==0) { printf "# TYPE top_process_cpu_usage gauge\n# HELP top_process_cpu_usage ps gathered instant CPU usage per process\n"; cputype=1 };
-                printf "top_process_cpu_usage{pid=\""$1"\",process=\""$12"\",sanitized_args=\""args"\"} " $9z"\n"};
+                printf "top_process_cpu_usage{pid=\""$1"\",process=\""$12"\",args=\""args"\"} " $9z"\n"};
         # Do not keep not memory hungry entries
         if ($10!="0.0") {
-                if (memtype==0) { printf "# TYPE top_process_memory_usage gauge\n# HELP top_process_memory_usage ps gathered memory usage per process\n"; memtype=1 };
-                printf "top_process_memory_usage{pid=\""$1"\",process=\""$12"\",sanitized_args=\""args"\"} " $10z"\n"};
+                printf "top_process_memory_usage{pid=\""$1"\",process=\""$12"\",args=\""args"\"} " $10z"\n"};
 }'
